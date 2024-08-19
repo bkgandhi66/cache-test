313965f6654cfa05e2cd8f941b0b0e54c995da7f
bkgandhi66 <bhakti.gandhi@orium.com>
2024-08-19 10:50:26 -0400

    Initial commit

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,44 @@
+import { useRouter } from 'next/router';
+
+export default function Post({ post }:any) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  // Example paths that you might fetch from an external source
+  const posts = [
+    { id: 'post-1' },
+    { id: 'post-2' },
+  ];
+
+  const paths = posts.map((post) => ({
+    params: { slug: post.id },
+  }));
+
+  return { paths, fallback: 'blocking' }; // `fallback: true` allows statically generated fallback pages
+}
+
+export async function getStaticProps({ params }:any) {
+  // Fetch post data based on slug
+  const posts:any = {
+    'post-1': { title: 'Post 1', content: 'This is the content of post 1.' },
+    'post-2': { title: 'Post 2', content: 'This is the content of post 2.' },
+  };
+
+  return {
+    props: {
+      post: posts?.[params?.slug] || null,
+    },
+    revalidate:120
+  };
+}
