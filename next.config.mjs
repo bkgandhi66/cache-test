e90baa84002a4efd8d050f6f37497d395fa69644
bkgandhi66 <bhakti.gandhi@orium.com>
2024-08-19 13:41:55 -0400

    Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:slug',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-max-age=123',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
